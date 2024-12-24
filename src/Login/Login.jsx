f02260b39feddeeea27543ce8a5e4d7482ef89dd
kamaubryan <briankamau344@gmail.com>
2024-12-24 22:13:25 +0300

    rectified some changes

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,74 +1,72 @@
-import React from "react";
+import {Link} from "react-router-dom";
 import google from "/assets/google-color-svgrepo-com.png";
-import Link from "react-router-dom";
+import SignUp from "../SignUp/SignUp";
 
 function Login() {
   return (
-    <div className="w-full flex justify-center items-center h-[100vh] ">
+    <div className="w-full flex justify-center items-center h-[100vh]">
       <div
-        className="loginPage  flex flex-col rounded-[20px] w-[460px] h-[550px] p-3  justify-between"
+        className="loginPage flex flex-col rounded-[20px] w-[460px] h-[550px] p-5 justify-between"
         id="login"
         style={{
           backgroundColor: "#ced4da",
         }}
       >
-        <h1 className="text-[40px] text-black flex  font-noto-sans-mono justify-center ">
+        <h1 className="text-[40px] text-black font-noto-sans-mono text-center">
           Welcome Back!
         </h1>
-        <h1></h1>
-        <div className="flex flex-col w-[400px] items-center h-[ 100px] gap-4">
-          <div className="email flex flex-col h-[80px] gap-2 ">
-            <label htmlFor="" className="border-">
-              Email
-            </label>
+
+        <div className="flex flex-col w-[400px] items-center h-[200px] gap-4">
+          <div className="email flex flex-col h-[80px] gap-2">
+            <label htmlFor="email" className="text-black">Email</label>
             <input
-              type="text"
-              name=""
-              id=""
-              placeholder="email"
-              className=" w-[350px] h-[40px] p-2 rounded-[15px] border-none "
-              color="#000814"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              className="w-[350px] h-[40px] p-2 rounded-[15px] border-none focus:outline-none"
             />
           </div>
+
           <div className="password flex flex-col h-[80px] gap-2">
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password" className="text-black">Password</label>
             <input
               type="password"
-              name=" password"
-              id=""
-              placeholder="password"
-              className=" w-[350px] h-[40px] p-2 rounded-[15px] "
-              color="#000814"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              className="w-[350px] h-[40px] p-2 rounded-[15px] border-none focus:outline-none"
             />
           </div>
         </div>
-        <div className="checkbox w-[400px] flex items-center px-3 justify-around">
-          <div className="input w-[200px] flex gap-2 ">
-            <input type="checkbox" name="Rememberme" id="" />
-            <label htmlFor="Rememberme">Remember me</label>
+
+        <div className="checkbox w-[400px] flex items-center justify-between px-3">
+          <div className="input flex items-center gap-2">
+            <input type="checkbox" name="rememberMe" id="rememberMe" />
+            <label htmlFor="rememberMe" className="text-black">Remember me</label>
           </div>
-          <p>Forgot Password</p>
+          <p className="text-black">Forgot Password</p>
         </div>
-        <div className="buttons w-[370px] flex items-center justify-between flex-col h-[90px] px-8">
-          <button className=" bg-black text-white w-[350px]  h-[35px] rounded-[15px] text-[18px] ">
-            {" "}
+
+        <div className="buttons w-[370px] flex items-center justify-between flex-col h-[90px] px-8 gap-4">
+          <button className="bg-black text-white w-[350px] h-[40px] rounded-[15px] text-[18px]">
             Sign in
           </button>
-          <button className=" bg-white text-black w-[350px]  h-[35px] rounded-[15px] text-[18px]  flex items-center justify-center gap-2">
-            {" "}
-            <img src={google} className="w-[20px] h-[20px]" alt="" /> Sign in
-            with Google
+
+          <button className="bg-white text-black w-[350px] h-[40px] rounded-[15px] text-[18px] flex items-center justify-center gap-2">
+            <img src={google} className="w-[20px] h-[20px]" alt="Google Logo" />
+            Sign in with Google
           </button>
         </div>
-        <div className="footer flex align-bottom justify-center items-center">
-          {" "}
-          Don't have an account?{" "}
+
+        {/* <div className="footer flex justify-center items-center mt-4">
+          <span>Don't have an account? </span>
           <span className="text-blue-600 mx-2">
-            <Link to="/signup" className="text-blue-500">
-              Sign up
+            <Link to="/" className="text-blue-500">
+             <SignUp/>
             </Link>
           </span>
-        </div>
+        </div> */}
       </div>
     </div>
   );
