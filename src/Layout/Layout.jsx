a1544a641cfa4b448b9a259d3bece0d1b24e156e
kamaubryan <briankamau344@gmail.com>
2024-12-30 22:40:34 +0300

    copied an inspo from my projects

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from '../Pages/LandingPage/LandingPage'
+
+function Layout() {
+  return (
+  <>
+  <LandingPage/>
+  </>
+  )
+}
+
+export default Layout
