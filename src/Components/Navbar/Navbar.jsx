9c228c79fa2887e9927c0e6c6da7708668b5980b
kamaubryan <briankamau344@gmail.com>
2025-01-01 12:17:47 +0300

    modified the navbar adding some colors to it

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "/assets/merc.jpg";
+import "./Navbar.css"
+import Home from "../../Pages/LandingPage/LandingPage"
+// import LandingPage from "../../Pages/LandingPage/LandingPage";
 
 function Navbar() {
   return (
     <>
       <nav
-        className="Navbar w-[95%] bg-black mx-auto flex items-center justify-between 
+        className="Navbar w-[95%] bg-white mx-auto flex items-center justify-between  
       "
       >
         <div className="logo">
-          <img src={logo} alt="" width="40px" height="40px" />
+          <Link to="/home">
+            <img src={logo} alt="" width="40px" height="60px"  />
+          </Link>
         </div>
-        <div className="navmenu">
-          <p>
-            <Link to="/home">Evaluation</Link>
+        <div className="navmenu flex items-center justify-between w-[40%]">
+          <p className="">
+            <Link to="/evaluation">Evaluation</Link>
           </p>
           <p>
-            <Link to="/Services">Services</Link>
+            <Link to="/services">Services</Link>
           </p>
           <p>
             <Link to="/about">About Us </Link>
@@ -25,14 +30,21 @@ function Navbar() {
           <p>
             <Link to="/settings">Settings</Link>
           </p>
+          <p>
+            <Link to="/contact">Contact Us</Link>
+          </p>
         </div>
-        <div className="authentication">
-          <button className="bg-blue-50 ">Login</button>
-          <button>Sign Up</button>
+        <div className="authentication w-[8%] flex items-center justify-between">
+          <button className="bg-blue-50 ">
+            <Link to="/login">Login</Link>
+          </button>
+          <button>
+            <Link to="/signUp"> Signup</Link>
+          </button>
         </div>
       </nav>
     </>
   );
 }
 
-export default Navbar
+export default Navbar;
