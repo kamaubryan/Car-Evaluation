1b64097cba9443ac4116272ec489fcdd53d7fda7
kamaubryan <briankamau344@gmail.com>
2025-01-01 12:18:42 +0300

    Modified the landing page

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import LandingPage from '../Pages/LandingPage/LandingPage'
 import Navbar from '../Components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer/Footer'
 
 function Layout() {
   return (
-  <>
+  <div className='p-5'>
  <Navbar/>
  <Outlet/>
+ <Footer/>
  
-  </>
+  </div>
   )
 }
 
