bdf049af14358e0c622198fec6d51095ee57b661
kamaubryan <briankamau344@gmail.com>
2025-01-02 22:31:06 +0300

    added a responsive effect

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,49 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "/assets/merc.jpg";
-import "./Navbar.css"
-import Home from "../../Pages/LandingPage/LandingPage"
-// import LandingPage from "../../Pages/LandingPage/LandingPage";
+import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi"; // Import hamburger icon
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <>
-      <nav
-        className="Navbar w-[95%] bg-white mx-auto flex items-center justify-between  
-      "
+    <nav className="Navbar w-[95%] bg-white mx-auto flex items-center justify-between p-4">
+      <div className="logo">
+        <Link to="/home">
+          <img src={logo} alt="logo" width="40px" height="60px" />
+        </Link>
+      </div>
+
+      {/* Hamburger Icon for Small Devices */}
+      <div className="md:hidden flex items-center">
+        <button onClick={toggleMenu} className="text-2xl">
+          <GiHamburgerMenu />
+        </button>
+      </div>
+
+      {/* Navigation Menu */}
+      <div
+        className={`navmenu flex items-center justify-between w-[40%] md:w-auto md:flex-row ${
+          isMenuOpen ? "flex-col" : "hidden"
+        } md:flex md:space-x-6`}
       >
-        <div className="logo">
-          <Link to="/home">
-            <img src={logo} alt="" width="40px" height="60px"  />
-          </Link>
-        </div>
-        <div className="navmenu flex items-center justify-between w-[40%]">
-          <p className="">
-            <Link to="/evaluation">Evaluation</Link>
-          </p>
-          <p>
-            <Link to="/services">Services</Link>
-          </p>
-          <p>
-            <Link to="/about">About Us </Link>
-          </p>
-          <p>
-            <Link to="/settings">Settings</Link>
-          </p>
-          <p>
-            <Link to="/contact">Contact Us</Link>
-          </p>
-        </div>
-        <div className="authentication w-[8%] flex items-center justify-between">
-          <button className="bg-blue-50 ">
-            <Link to="/login">Login</Link>
-          </button>
-          <button>
-            <Link to="/signUp"> Signup</Link>
-          </button>
-        </div>
-      </nav>
-    </>
+        <p>
+          <Link to="/evaluation">Evaluation</Link>
+        </p>
+        <p>
+          <Link to="/services">Services</Link>
+        </p>
+        <p>
+          <Link to="/about">About Us</Link>
+        </p>
+        <p>
+          <Link to="/settings">Settings</Link>
+        </p>
+        <p>
+          <Link to="/contact">Contact Us</Link>
+        </p>
+      </div>
+
+      {/* Authentication Buttons */}
+      <div className="authentication w-[8%] flex items-center justify-between">
+        <button className="bg-blue-50">
+          <Link to="/login">Login</Link>
+        </button>
+        <button>
+          <Link to="/signUp">Signup</Link>
+        </button>
+      </div>
+    </nav>
   );
 }
 
