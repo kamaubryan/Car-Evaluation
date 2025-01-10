656cfda8d3873f52b1eed6ca8b111dfdc69724da
kamaubryan <briankamau344@gmail.com>
2025-01-10 19:48:16 +0300

    sorted the files to frontend and backend

---
+++ salvagebackend/src/main/java/com/example/salvagebackend/SalvagebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.salvagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalvagebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvagebackendApplication.class, args);
+	}
+
+}
---
+++ salvagebackend/src/test/java/com/example/salvagebackend/SalvagebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salvagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalvagebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
