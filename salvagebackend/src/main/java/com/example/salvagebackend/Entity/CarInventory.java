539bff1f1c4e01d1366076221364513cfa0e5265
kamaubryan <briankamau344@gmail.com>
2025-01-14 20:02:58 +0300

    became stuck since user entity is not being created

--- salvagebackend/src/main/java/com/example/salvagebackend/Entity/CarInventory.java
+++ salvagebackend/src/main/java/com/example/salvagebackend/Entity/CarInventory.java
@@ -5,18 +5,29 @@ import jakarta.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "Car_Inventory")
+@Table
+////@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+//@DiscriminatorColumn(name = "car_type", discriminatorType = DiscriminatorType.STRING)
 public class CarInventory extends BaseEntity {
+
+  @Column(name = "Car_Name")
   private String title;
+  @Column(name = "Selling_Price")
   private BigDecimal  sellingPrice;
-  private String descriptionDescription;
+  @Column(name = "Damage_Description")
+  private String damageDescription;
+  @Column(name = "Car_Make")
   private String make;
+  @Column(name = "Car_Model")
   private String model;
-  private int year;
+  @Column(name = "YearOfMake")
+  private Integer year;
+  @Column(name = "Mileage")
   private Integer mileage;
+  @Column(name = "Car_Condition")
   private String vehicleCondition;
 
-  @Enumerated(EnumType.STRING)
+//  @Enumerated(EnumType.STRING)
 //  private InventoryStatus status = Inventory.Available;
   private boolean featured;
 
@@ -37,11 +48,11 @@ public class CarInventory extends BaseEntity {
   }
 
   public String getDescriptionDescription() {
-    return descriptionDescription;
+    return damageDescription;
   }
 
   public void setDescriptionDescription(String descriptionDescription) {
-    this.descriptionDescription = descriptionDescription;
+    this.damageDescription = descriptionDescription;
   }
 
   public String getMake() {
