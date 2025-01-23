6ac9261937c901a038d8aa6d7c8e37f28689768f
kamaubryan <briankamau344@gmail.com>
2025-01-23 19:42:53 +0300

    added the customized sign up page

--- SalvageFrontEnd/src/Pages/SignUp/SignUp.jsx
+++ SalvageFrontEnd/src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,199 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function SignUp() {
+import { Button, Form, Grid, Input, theme, Typography } from "antd";
+
+import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const { useToken } = theme;
+const { useBreakpoint } = Grid;
+const { Text, Title } = Typography;
+
+export default function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // State to manage error messages
+  const history = useNavigate(); // Get the history object for redirection
+
+  const handleSignup = async () => {
+    try {
+      // Check for empty fields
+      if (
+        !fistName ||
+        secondddddd ||
+        !email ||
+        !password ||
+        !confirmPassword ||
+        !mobile
+      ) {
+        setError("Please fill in all fields.");
+        return;
+      }
+
+
+      const response = await axios.post(
+        "http://localhost:8081/api/v1/auth/signup",
+        {
+          firstName,
+          secondName,
+          email,
+          password,
+        }
+      );
+      // Handle successful signup
+      console.log(response.data);
+      history("/landingPage");
+    } catch (error) {
+      // Handle signup error
+      console.error(
+        "Signup failed:",
+        error.response ? error.response.data : error.message
+      );
+      setError(error.response ? error.response.data : error.message);
+    }
+  };
+  const { token } = useToken();
+  const screens = useBreakpoint();
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  const styles = {
+    container: {
+      margin: "0 auto",
+      padding: screens.md
+        ? `${token.paddingXL}px`
+        : `${token.paddingXL}px ${token.padding}px`,
+      width: "380px",
+    },
+    forgotPassword: {
+      float: "right",
+    },
+    header: {
+      marginBottom: token.marginXL,
+      textAlign: "center",
+    },
+    section: {
+      alignItems: "center",
+      backgroundColor: token.colorBgContainer,
+      display: "flex",
+      height: screens.sm ? "100vh" : "auto",
+      padding: screens.md ? `${token.sizeXXL}px 0px` : "0px",
+    },
+    signup: {
+      marginTop: token.marginLG,
+      textAlign: "center",
+      width: "100%",
+    },
+    text: {
+      color: token.colorTextSecondary,
+    },
+    title: {
+      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3,
+    },
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <section style={styles.section}>
+      <div style={styles.container}>
+        <div style={styles.header}>
+          <Title style={styles.title}>Sign up</Title>
+          <Text style={styles.text}>
+            Join us! Create an account to get started.
+          </Text>
+        </div>
+        <Form
+          name="normal_signup"
+          onFinish={onFinish}
+          layout="vertical"
+          requiredMark="optional"
+        >
+          <Form.Item
+            name="First Name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your  Second Name!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="FirstName"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="Second Name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your second Name!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Second Name"
+              onChange={(e) => {
+                setSecondName(e.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: "email",
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined />}
+              placeholder="Email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            extra="Password needs to be at least 8 characters."
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item style={{ marginBottom: "0px" }}>
+            <Button block type="primary" htmlType="submit">
+              Sign up
+            </Button>
+            <div style={styles.signup}>
+              <Text style={styles.text}>Already have an account?</Text>{" "}
+              <Link to="/login">Sign in</Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
+  );
 }
-
-export default SignUp
