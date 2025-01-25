56e2340d69db1af7d51af5c61d6ffbe9231b04fb
kamaubryan <briankamau344@gmail.com>
2025-01-25 22:37:53 +0300

    added some configurstion on the path on the auth controller

--- salvagebackend/src/main/java/com/example/salvagebackend/Configurations/SecurityConfig.java
+++ salvagebackend/src/main/java/com/example/salvagebackend/Configurations/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
+<<<<<<< Updated upstream
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 //            http.cors(cors -> cors.configurationSource(request -> {
@@ -55,6 +56,29 @@ public class SecurityConfig {
                 .authenticationManager(authenticationManager)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+=======
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.cors(cors -> cors.configurationSource(request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowCredentials(true);
+                        config.addAllowedOriginPattern("*");
+                        config.addAllowedHeader("*");
+                        config.addAllowedMethod("*");
+                        return config;
+                    }));
+                 http.csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                         .authorizeHttpRequests((requests) -> requests
+                                 .requestMatchers("/api/v1/auth/**").permitAll()  // Keep this for auth endpoints
+                                 .requestMatchers("/api/v1/cars/**").authenticated()  // Cars should be authenticated
+                                 .requestMatchers("/api/v1/parts/**").authenticated()
+                                 .requestMatchers("/api/v1/users/**").authenticated()
+                                 .requestMatchers("/api/transactions/**").authenticated()
+                                 .anyRequest().authenticated()
+                         )
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+>>>>>>> Stashed changes
 
     }
 
