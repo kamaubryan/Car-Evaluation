54064578c58399ef96728595c869accdcb558cff
kamaubryan <briankamau344@gmail.com>
2025-01-28 19:18:25 +0300

    wrapped the function with a config provider for the customization of the ant D

--- SalvageFrontEnd/src/App.jsx
+++ SalvageFrontEnd/src/App.jsx
@@ -1,12 +1,26 @@
+import { ConfigProvider } from "antd";
 import Routes from "./Routes/Route";
-
+import { ToastContainer } from "react-toastify";
+import { CartProvider } from "./context/contextProvider"; // Make sure to update this import path
 
 function App() {
   return (
-<>
-<Routes/>
-
-</>
+    <CartProvider>
+      <ConfigProvider
+        cssVar
+        true
+        theme={{
+          "primary-color": "#1890ff",
+        }}
+      >
+        <Routes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={true}
+        />
+      </ConfigProvider>
+    </CartProvider>
   );
 }
 
