72dd58b34b9b6984d3d1be3b429c565303713ebc
kamaubryan <briankamau344@gmail.com>
2025-01-29 22:44:47 +0300

    added a new background image

--- salvagebackend/src/main/java/com/example/salvagebackend/Repository/CarImageRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.salvagebackend.Repository;
-
-import com.example.salvagebackend.Entity.CarImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarImageRepo extends JpaRepository<CarImage, Long> {}
